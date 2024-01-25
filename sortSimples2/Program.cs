76c7b0226064bc2b38bf8525622debfbb21923af
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-25 19:02:58 -0300

    Outra forma de resolver, Sort Simples

---
+++ sortSimples2/Program.cs
@@ -0,0 +1,41 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1042
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] entrada = Console.ReadLine().Split(' ');
+        int a = int.Parse(entrada[0]);
+        int b = int.Parse(entrada[1]);
+        int c = int.Parse(entrada[2]);
+
+
+        int menor = Math.Min(Math.Min(a, b), c);
+        int maior = Math.Max(Math.Max(a, b), c);
+        int meio;
+
+        if (a != maior && a != menor)
+        {
+            meio = a;
+        }
+        else if (b != menor && b != maior)
+        {
+            meio = b;
+        }
+        else
+        {
+            meio = c;
+        }
+
+        Console.WriteLine(menor);
+        Console.WriteLine(meio);
+        Console.WriteLine(maior);
+        Console.WriteLine();
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+
+    }
+}
