9a2b41136f2d22ac9af777d63316ae9b52ca00f8
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-25 18:30:56 -0300

    Exercicios Sort Simples e Distancia Entre Dois Pontos

---
+++ distanciaEntreDoisPontos/Program.cs
@@ -0,0 +1,32 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1015
+
+using System;
+
+namespace DistanciaEntreDoisPontos_1015;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        string[] entradas = Console.ReadLine().Split(' ');
+
+        double x1 = double.Parse(entradas[0]);
+        double y1 = double.Parse(entradas[1]);
+
+        entradas = Console.ReadLine().Split(' ');
+
+        double x2 = double.Parse(entradas[0]);
+        double y2 = double.Parse(entradas[1]);
+
+        double diferencaX = x2 - x1;
+        double diferencaY = y2 - y1;
+
+        double diferencaEntrePontos = Math.Sqrt(diferencaX
+            * diferencaX + diferencaY * diferencaY);
+
+        Console.WriteLine($"{diferencaEntrePontos:F4}");
+
+    }
+
+}
---
+++ sortSimples/Program.cs
@@ -0,0 +1,69 @@
+﻿//existem outras formas de fazer, como usando min/max etc, mas vou manter essa por enquanto
+//https://www.beecrowd.com.br/judge/pt/problems/view/1042
+
+using System;
+public class Program
+{
+
+    public static void Main()
+    {
+        string[] entradas = Console.ReadLine().Split(' ');
+        int entrada1 = int.Parse(entradas[0]);
+        int entrada2 = int.Parse(entradas[1]);
+        int entrada3 = int.Parse(entradas[2]);
+
+        if (entrada1 < entrada2 && entrada1 < entrada3)
+        {
+            Console.WriteLine($"{entrada1}");
+
+            if (entrada2 < entrada3)
+            {
+                Console.WriteLine($"{entrada2}");
+                Console.WriteLine($"{entrada3}");
+            }
+            else
+            {
+                Console.WriteLine($"{entrada3}");
+                Console.WriteLine($"{entrada2}");
+            }
+        }
+
+        if (entrada2 < entrada3 && entrada2 < entrada1)
+        {
+            Console.WriteLine($"{entrada2}");
+            if (entrada3 < entrada1)
+            {
+                Console.WriteLine($"{entrada3}");
+                Console.WriteLine($"{entrada1}");
+            }
+            else
+            {
+                Console.WriteLine($"{entrada1}");
+                Console.WriteLine($"{entrada3}");
+            }
+
+        }
+
+        if (entrada3 < entrada1 && entrada3 < entrada2)
+        {
+            Console.WriteLine($"{entrada3}");
+            if (entrada2 < entrada1)
+            {
+                Console.WriteLine($"{entrada2}");
+                Console.WriteLine($"{entrada1}");
+
+            }
+            else
+            {
+                Console.WriteLine($"{entrada1}");
+                Console.WriteLine($"{entrada2}");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"{entrada1}");
+        Console.WriteLine($"{entrada2}");
+        Console.WriteLine($"{entrada3}");
+
+    }
+}
