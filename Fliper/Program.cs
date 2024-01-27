af00f21cf0be7beefad378b73bdfe519326038d6
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-27 14:06:52 -0300

    Exercicio Fliper resolvido

---
+++ Fliper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        string[] entradas = Console.ReadLine().Split(' ');
+        int portaP = int.Parse(entradas[0]);
+        int portaR = int.Parse(entradas[1]);
+
+        if (portaP == 0)
+        {
+            Console.WriteLine("C");
+        }
+        else if (portaP == 1 && portaR == 1)
+        {
+            Console.WriteLine("A");
+        }
+        else
+        {
+            Console.WriteLine("B");
+        }
+
+    }
+
+}
