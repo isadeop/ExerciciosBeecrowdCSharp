b9f0811f9408b3717c0749d0a47492f4adc46f46
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-27 14:19:05 -0300

    Exercício Tabuada Resolvido

---
+++ Tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1078
+
+using System;
+
+public class Program
+{
+
+    static void Main()
+    {
+
+        int entradaN = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+
+            int tabuada = i * entradaN;
+
+            Console.WriteLine($"{i} x {entradaN} = {tabuada}");
+        }
+
+    }
+
+}
