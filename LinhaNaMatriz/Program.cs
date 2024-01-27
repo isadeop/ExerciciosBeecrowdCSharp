deceea2ee4e75b57bfc7bb0b028c1e20752ccddf
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-27 16:50:50 -0300

    Exercício Linha Na Matriz resolvido

---
+++ LinhaNaMatriz/Program.cs
@@ -0,0 +1,40 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1181
+
+using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        int entrada = int.Parse(Console.ReadLine());
+        string tipo = Console.ReadLine();
+        double valores = 0.0;
+
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                double elemento = double.Parse(Console.ReadLine());
+
+                if (i == entrada)
+                {
+                    valores += elemento;
+                }
+            }
+        }
+
+        if (tipo == "S")
+        {
+            Console.WriteLine($"{valores:F1}");
+        }
+        else if (tipo == "M")
+        {
+            double media = valores / 12;
+            Console.WriteLine($"{media:F1}");
+        }
+
+    }
+
+}
