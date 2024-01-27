114e168ee0b12d02bfb52d2d5b22253d4a00d4f5
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-27 14:34:02 -0300

    Exercício Fatorial Simples Resolvido

---
+++ Fatorial/Program.cs
@@ -0,0 +1,23 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1153
+
+using System;
+
+public class Program
+{
+
+    static void Main()
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int fatorial = 1;
+
+        for (int i = n; i > 0; i--)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine(fatorial);
+
+    }
+
+}
