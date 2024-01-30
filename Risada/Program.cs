697bb340d4e23a93a28dc88122fb5ef08939b188
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2024-01-30 12:33:13 -0300

    Exercicio Risada Resolvido

---
+++ Risada/Program.cs
@@ -0,0 +1,43 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/2242
+
+using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        string entrada = Console.ReadLine();
+        string vogais = "";
+        string vogaisInvertidas = "";
+
+        for (int i = 0; i < entrada.Length; i++)
+        {
+            switch (entrada[i])
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vogais += entrada[i];
+                    break;
+            }
+        }
+
+        for (int j = vogais.Length - 1; j >= 0; j--)
+        {
+            vogaisInvertidas += vogais[j];
+        }
+
+        if (vogais == vogaisInvertidas)
+        {
+            Console.WriteLine("S");
+        }
+        else
+        {
+            Console.WriteLine("N");
+        }
+    }
+}
